f48d4830ad850d025e477f361de22d49ae8d3da1
Calvine Akoonyatse <cakoonyatse@gmail.com>
2022-05-04 23:11:19 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ class App extends React.Component {
         this.state = {
             buttonClicked: "",
             assignments: [] /*Below this line, add the students state variable*/ ,
-            grades: {}
+            students [],
         };
 
         this.handleButtonClicked = this.handleButtonClicked.bind(this);
